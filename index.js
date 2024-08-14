9b41741511394544d672b7cd7e2ebb79b0bcc0bf
knurzhanov <knurzhanov1999@gmail.com>
2024-08-14 22:00:52 +0600

    3 commit

--- index.js
+++ index.js
@@ -11,15 +11,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
-
 mongoose.connect(process.env.MONGO_URI, {
   // useNewUrlParser: true, // Удалите это
   // useUnifiedTopology: true, // Удалите это
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
-
 // Модель недвижимости
 const PropertySchema = new mongoose.Schema({
   title: { type: String, required: true },
