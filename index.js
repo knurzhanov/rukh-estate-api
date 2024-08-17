9eb506a1dc6b0e5d7d0cdd8f90b0939ccb0f2e10
knurzhanov <knurzhanov1999@gmail.com>
2024-08-18 02:11:06 +0500

    cors fix

--- index.js
+++ index.js
@@ -103,7 +103,13 @@ app.get('/properties/:id', async (req, res) => {
   }
 });
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://rukh-estate.vercel.app/', // Укажите домен фронтенда
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешенные методы
+  allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
+}));
+
+
 // Запуск сервера
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
