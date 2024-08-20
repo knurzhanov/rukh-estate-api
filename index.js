ccec65d840046a2d28a58e2087a3d28d9c8a485a
knurzhanov <knurzhanov1999@gmail.com>
2024-08-20 23:06:44 +0600

    fix

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.get('/properties', async (req, res) => {
     if (properties.length > 0) {
       res.json(properties);
     } else {
+      res.status(200).send('Нет квартир с таким количеством комнат');
       res.status(404).send('Нет доступных квартир');
     }
   } catch (error) {
